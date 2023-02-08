f6f14b463f5fec5201bc8fdf75c5d736feb4252c
Ben Greanya <bengreanya@gmail.com>
2023-02-08 10:04:36 -0800

    auth working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Auth from './components/Auth.js';
+import Posts from './components/Posts.js';
 
 function App() {
   return (
     <div className="App">
-      <h1>CRUDDDDDDD</h1>
+      {/* <Switch>
+        <Route path="/posts" component={Posts} />
+        <Route path="/auth/:type" component={Auth} />
+        <Route path="*" />
+        <Redirect to="/auth/sign-in" />
+      </Switch> */}
+      <Auth />
     </div>
   );
 }
---
+++ src/context/UserContext.js
@@ -0,0 +1,19 @@
+import { createContext, useContext, useState } from 'react';
+import { getUser } from '../services/auth.js';
+
+const UserContext = createContext();
+const UserProvider = ({ children }) => {
+  const currentUser = getUser();
+  const [user, setUser] = useState(currentUser);
+  return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>;
+};
+
+const useUser = () => {
+  const context = useContext(UserContext);
+  if (context === undefined) {
+    throw new Error('useUser must be used with UserProvider');
+  }
+  return context;
+};
+
+export { UserProvider, useUser };
