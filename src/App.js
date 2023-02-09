27689569b00c59bf1ea10d2078c37b91e8f60594
Ben Greanya <bengreanya@gmail.com>
2023-02-09 10:53:38 -0800

    added header with create that redirects to newpost

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Auth from './components/Auth.js';
+import Header from './components/Header.js';
 import NewPost from './components/NewPost.js';
 import Posts from './components/Posts.js';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/posts" component={Posts} />
         <Route exact path="/auth/:type" component={Auth} />
